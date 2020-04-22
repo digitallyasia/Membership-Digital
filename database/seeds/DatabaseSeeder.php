1092aa7fb2ab3bcefe13326f5b946c66da520f54
Faiz Ahmad <faizahmadse@gmail.com>
2020-04-22 16:50:48 +0500

    installation and configuration

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        factory('App\User')->create([
+            'name' => 'Admin',
+            'email' => 'admin@ngo.com'
+        ]);
     }
 }
