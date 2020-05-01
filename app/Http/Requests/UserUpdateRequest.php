9ab9b1c761c98882ddaa3a8bf16a5a37f4cf95ce
Faiz Ahmad <faizahmadse@gmail.com>
2020-05-01 21:25:06 +0500

    Create UserUpdateRequest.php

---
+++ app/Http/Requests/UserUpdateRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'phone_number' => 'required|string',
+            'profile_picture' => 'required|string',
+            'user_name' => 'required|string',
+            'fcm_token' => 'required|string',
+            'address' => 'required|string',
+            'date_of_birth' => 'required',
+        ];
+    }
+}
