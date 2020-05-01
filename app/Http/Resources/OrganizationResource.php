772ac9b98d2925982d8f693c6e7607aca745b90a
Faiz Ahmad <faizahmadse@gmail.com>
2020-05-01 21:25:13 +0500

    Create OrganizationResource.php

---
+++ app/Http/Resources/OrganizationResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrganizationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'logo' => $this->logo ? \Storage::disk('images')->url(
+                $this->logo
+            ) : null,
+            'description' => $this->description,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'address' => $this->address,
+            'city' => $this->city,
+            'state' => $this->state,
+            'postal_code' => $this->postal_code,
+            'status' => $this->status,
+        ];
+    }
+}
