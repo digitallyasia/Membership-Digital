565c23b8de0de1af6e2c4968eb5896f0f71f2b53
Faiz Ahmad <faizahmadse@gmail.com>
2020-05-03 04:47:30 +0500

    Create AnnouncementRequest.php

---
+++ app/Http/Requests/AnnouncementRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AnnouncementRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $image = request()->hasFile('image') ? 'required|image|mimes:jpeg,jpg,png|max:2048' : 'nullable';
+        return [
+            'title' => ['required', 'string', 'max:255'],
+            'details' => ['required', 'string'],
+            'url' => ['nullable', 'string', 'active_url'],
+            'image' => $image,
+        ];
+    }
+}
