237de4b916616cd34016f9ef1feeded067230f27
Faiz Ahmad <faizahmadse@gmail.com>
2020-05-11 02:25:39 +0500

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,10 +14,10 @@ $factory->define(User::class, function (Faker $faker) {
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
         'phone_number' => $faker->phoneNumber,
-        'profile_picture' => $faker->word,
+        'profile_picture' => 'http://placeimg.com/200/200/tech',
         'user_name' => $faker->userName,
         'fcm_token' => $faker->word,
-        'address' => $faker->word,
+        'address' => $faker->streetAddress,
         'date_of_birth' => $faker->dateTime(),
     ];
 });
