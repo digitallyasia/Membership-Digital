b7c28070854343745517ddfba217a183a1356ed5
Faiz Ahmad <faizahmadse@gmail.com>
2020-05-11 02:19:30 +0500

    Create OrganiztionAnnouncementsController.php

---
+++ app/Http/Controllers/Organization/OrganiztionAnnouncementsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Organization;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
+use Inertia\Inertia;
+
+class OrganiztionAnnouncementsController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        return Inertia::render('Organization/Announcements', [
+            'filters' => Request::all('search', 'trashed'),
+            'announcements' => Auth::guard('organization')->user()->announcements()
+                ->filter(Request::only('search', 'trashed'))
+                ->paginate()
+        ]);
+    }
+}
