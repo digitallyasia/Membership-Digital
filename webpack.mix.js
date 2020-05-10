1f44577c49a758eab31f46b83e453e1ad52a69e0
Faiz Ahmad <faizahmadse@gmail.com>
2020-05-11 02:25:23 +0500

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,9 @@
-const mix = require('laravel-mix');
+const cssImport = require("postcss-import");
+const cssNesting = require("postcss-nesting");
+const mix = require("laravel-mix");
+const path = require("path");
+const purgecss = require("@fullhuman/postcss-purgecss");
+const tailwindcss = require("tailwindcss");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +16,36 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.js("resources/js/app.js", "public/js")
+    .postCss("resources/css/app.css", "public/css/app.css")
+    .options({
+        postCss: [
+            cssImport(),
+            cssNesting(),
+            tailwindcss("tailwind.config.js"),
+            ...(mix.inProduction()
+                ? [
+                      purgecss({
+                          content: [
+                              "./resources/views/**/*.blade.php",
+                              "./resources/js/**/*.vue"
+                          ],
+                          defaultExtractor: content =>
+                              content.match(/[\w-/:.]+(?<!:)/g) || [],
+                          whitelistPatternsChildren: [/nprogress/]
+                      })
+                  ]
+                : [])
+        ]
+    })
+    .webpackConfig({
+        output: { chunkFilename: "js/[name].js?id=[chunkhash]" },
+        resolve: {
+            alias: {
+                vue$: "vue/dist/vue.runtime.esm.js",
+                "@": path.resolve("resources/js")
+            }
+        }
+    })
+    .version()
+    .sourceMaps();
