b24b6bb20ca72f7f839410294b22114bf9d42acf
Faiz Ahmad <faizahmadse@gmail.com>
2020-05-11 02:26:02 +0500

    Update BenefitFactory.php

--- database/factories/BenefitFactory.php
+++ database/factories/BenefitFactory.php
@@ -9,8 +9,8 @@ $factory->define(Benefit::class, function (Faker $faker) {
     return [
         'organization_id' => factory(\App\Organization::class),
         'title' => $faker->sentence(4),
-        'details' => $faker->word,
-        'image' => $faker->word,
-        'promocode' => $faker->word,
+        'details' => $faker->sentence(10),
+        'image' => 'http://placeimg.com/400/400/tech',
+        'promo_code' => $faker->word,
     ];
 });
