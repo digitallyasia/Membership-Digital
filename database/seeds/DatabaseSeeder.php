544e1b0ef40e23a03f0bf6b9a798d28fde39ec60
Faiz Ahmad <faizahmadse@gmail.com>
2020-05-11 02:31:20 +0500

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,6 +4,7 @@ use App\Announcement;
 use App\Benefit;
 use App\Notification;
 use App\Organization;
+use App\Plan;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,6 +17,51 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        $plans = [
+            [
+                'name' => 'Free',
+                'code' => 'free',
+                'membership_number' => false,
+                'import_membership_csv' => false,
+                'card_customization' => false,
+                'number_of_notifications' => 20,
+                'number_of_announcements' => 20,
+                'number_of_benefits' => 20,
+                'number_of_members' => 100,
+                'monthly_price' => 0,
+                'yearly_price' => 0,
+            ],
+            [
+                'name' => 'Basic',
+                'code' => 'standard',
+                'membership_number' => false,
+                'import_membership_csv' => false,
+                'card_customization' => false,
+                'number_of_notifications' => 40,
+                'number_of_announcements' => 40,
+                'number_of_benefits' => 30,
+                'number_of_members' => 1000,
+                'monthly_price' => 100,
+                'yearly_price' => 900,
+            ],
+            [
+                'name' => 'Premium',
+                'code' => 'premium',
+                'membership_number' => true,
+                'import_membership_csv' => true,
+                'card_customization' => true,
+                'number_of_notifications' => 65,
+                'number_of_announcements' => 50,
+                'number_of_benefits' => 40,
+                'number_of_members' => 12000,
+                'monthly_price' => 200,
+                'yearly_price' => 1500,
+            ],
+
+        ];
+        foreach ($plans as $plan) {
+            Plan::Create($plan);
+        }
         factory('App\User')->create([
             'name' => 'Admin',
             'email' => 'admin@membershipdigital.my'
@@ -28,6 +74,6 @@ class DatabaseSeeder extends Seeder
         $organization->announcements()->saveMany(factory(Announcement::class, 20)->create());
         $organization->benefits()->saveMany(factory(Benefit::class, 20)->create());
         $organization->notifications()->saveMany(factory(Notification::class, 20)->create());
-        $organization->members()->attach($members, ['status' => true]);
+        $organization->members()->attach($members, ['status' => 'accepted']);
     }
 }
