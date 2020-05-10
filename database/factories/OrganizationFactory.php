fd96863df4f51c484f99ab01032ef9506fed1d48
Faiz Ahmad <faizahmadse@gmail.com>
2020-05-11 02:25:48 +0500

    Update OrganizationFactory.php

--- database/factories/OrganizationFactory.php
+++ database/factories/OrganizationFactory.php
@@ -8,14 +8,16 @@ use Faker\Generator as Faker;
 $factory->define(Organization::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'logo' => $faker->word,
+        'logo' => 'http://placeimg.com/200/200/tech',
         'description' => $faker->text,
         'email' => $faker->safeEmail,
+        'email_verified_at' => now(),
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token' => \Str::random(10),
         'phone' => $faker->phoneNumber,
-        'address' => $faker->word,
+        'address' => $faker->streetAddress,
         'city' => $faker->city,
-        'state' => $faker->word,
+        'state' => $faker->state,
         'postal_code' => $faker->postcode,
-        'user_id' => factory(\App\User::class),
     ];
 });
