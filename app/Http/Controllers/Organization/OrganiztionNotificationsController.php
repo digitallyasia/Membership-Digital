efb4a7b5071d02475ed67436c4f466d38872009c
Faiz Ahmad <faizahmadse@gmail.com>
2020-05-11 02:20:09 +0500

    Create OrganiztionNotificationsController.php

---
+++ app/Http/Controllers/Organization/OrganiztionNotificationsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Organization;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
+use Inertia\Inertia;
+
+class OrganiztionNotificationsController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        return Inertia::render('Organization/Notifications', [
+            'filters' => Request::all('search', 'trashed'),
+            'notifications' => Auth::guard('organization')->user()->notifications()
+                ->filter(Request::only('search', 'trashed'))
+                ->paginate()
+        ]);
+    }
+}
