8fc148212ded463059ea570b76d791733a1fd2f5
Faiz Ahmad <faizahmadse@gmail.com>
2020-05-12 23:54:18 +0500

    Update Login.vue

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="p-6 bg-indigo-800 min-h-screen flex justify-center items-center">
+  <div class="flex items-center justify-center min-h-screen p-6 bg-indigo-800">
     <div class="w-full max-w-md">
-      <logo class="block mx-auto w-full max-w-xs fill-white" height="50" />
-      <form class="mt-8 bg-white rounded-lg shadow-xl overflow-hidden" @submit.prevent="submit">
-        <div class="px-10 py-12">
-          <h1 class="text-center font-bold text-3xl">Welcome Back!</h1>
-          <div class="mx-auto mt-6 w-24 border-b-2" />
+      <logo class="block w-full max-w-xs mx-auto fill-white" height="50" />
+      <form class="mt-8 overflow-hidden bg-white rounded-lg shadow-xl" @submit.prevent="submit">
+        <div class="px-10 py-10">
+          <h1 class="text-3xl font-bold text-center">Welcome Back!</h1>
+          <div class="w-24 mx-auto mt-6 border-b-2" />
           <text-input
             v-model="form.email"
             :errors="$page.errors.email"
@@ -16,16 +16,29 @@
             autocapitalize="off"
           />
           <text-input v-model="form.password" class="mt-6" label="Password" type="password" />
-          <label class="mt-6 select-none flex items-center" for="remember">
-            <input id="remember" v-model="form.remember" class="mr-1" type="checkbox" />
-            <span class="text-sm">Remember Me</span>
-          </label>
+          <div class="flex items-center justify-between mt-6 mb-8">
+            <label class="flex items-center select-none" for="remember">
+              <input id="remember" v-model="form.remember" class="mr-1" type="checkbox" />
+              <span class="text-sm">Remember Me</span>
+            </label>
+            <inertia-link
+              class="font-bold hover:underline"
+              :href="route('organization.password.email')"
+            >Forget password?</inertia-link>
+          </div>
+          <loading-button
+            :loading="sending"
+            class="justify-center w-full text-2xl font-bold text-center btn-indigo"
+            type="submit"
+          >Login</loading-button>
         </div>
         <div
-          class="px-10 py-4 bg-gray-100 border-t border-gray-200 flex justify-between items-center"
+          class="flex items-center justify-between px-10 py-4 bg-gray-100 border-t border-gray-200"
         >
-          <a class="hover:underline" tabindex="-1" href="#reset-password">Forget password?</a>
-          <loading-button :loading="sending" class="btn-indigo" type="submit">Login</loading-button>
+          <inertia-link
+            class="font-bold hover:underline"
+            :href="route('organization.register')"
+          >Not a registered Organisation? Register here!</inertia-link>
         </div>
       </form>
     </div>
