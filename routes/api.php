a088ca910ccf4c37c8e72d7259f7f7d92b6d6890
Faiz Ahmad <faizahmadse@gmail.com>
2020-05-14 21:14:23 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -62,30 +62,24 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::prefix('organizations/{organization:uuid}')->group(function () {
         Route::get('/', function (App\Organization $organization, Request $request) {
-            if ($organization->membership($request->user())) {
-                return new OrganizationResource($request->user()->organizations()->where('organization_members.organization_id', '=', $organization->id)->first());
-            } else {
-                return new OrganizationResource($organization);
-            }
+            return $organization->membership($request->user())
+                ?   new OrganizationResource($request->user()->organizations()->where('organization_members.organization_id', '=', $organization->id)->first())
+                :   new OrganizationResource($organization);
         });
 
         Route::get('/join', 'API\OrganizationJoinController');
     });
     Route::prefix('organizations/{organization}/')->group(function () {
         Route::get('announcements', function (App\Organization $organization, Request $request) {
-            if ($organization->isMember($request->user())) {
-                return new AnnouncementCollection(Announcement::where('organization_id', $organization->id)->paginate());
-            } else {
-                return response(['message' => 'You are not member of this organization'], 403);
-            }
+            return $organization->isMember($request->user())
+                ? new AnnouncementCollection(Announcement::where('organization_id', $organization->id)->paginate())
+                : response(['message' => 'You are not member of this organization'], 403);
         });
 
         Route::get('benefits', function (App\Organization $organization, Request $request) {
-            if ($organization->isMember($request->user())) {
-                return new BenefitCollection(Benefit::where('organization_id', $organization->id)->paginate());
-            } else {
-                return response(['message' => 'You are not member of this organization'], 403);
-            }
+            return $organization->isMember($request->user())
+                ? new BenefitCollection(Benefit::where('organization_id', $organization->id)->paginate())
+                : response(['message' => 'You are not member of this organization'], 403);
         });
     });
 });
