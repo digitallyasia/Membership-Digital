1fcd483a62cfbdc3a1862a587a930fed4fd9583f
Faiz Ahmad <faizahmadse@gmail.com>
2020-05-30 17:23:56 +0500

    Update User.php

--- app/User.php
+++ app/User.php
@@ -74,9 +74,9 @@ class User extends Authenticatable
     public function getProfilePictureAttribute($value)
     {
         return $value !== null
-            ? preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $value)
-            ? $value
-            : Storage::disk('images')->url($value)
+            ? (preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $value)
+                ? $value
+                : Storage::disk('images')->url($value))
             : null;
     }
 }
