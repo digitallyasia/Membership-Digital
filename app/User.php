d8ab3dcb16c5e22ee6ace51a293a5cf9ec84a420
Faiz Ahmad <faizahmadse@gmail.com>
2020-05-30 16:40:54 +0500

    Update User.php

--- app/User.php
+++ app/User.php
@@ -71,4 +71,12 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(\App\Organization::class, 'organization_members')->withPivot('status');
     }
+    public function getProfilePictureAttribute($value)
+    {
+        return $value !== null
+            ? preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $value)
+            ? $value
+            : Storage::disk('images')->url($value)
+            : null;
+    }
 }
