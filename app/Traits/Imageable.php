8bf088ac1ea3bfead3ab5cc7008bc05c0edb510d
Faiz Ahmad <faizahmadse@gmail.com>
2020-05-30 17:23:33 +0500

    Update Imageable.php

--- app/Traits/Imageable.php
+++ app/Traits/Imageable.php
@@ -9,9 +9,9 @@ trait Imageable
     public function getImageAttribute($value)
     {
         return $value !== null
-            ? preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $value)
-            ? $value
-            : Storage::disk('images')->url($value)
+            ? (preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $value)
+                ? $value
+                : Storage::disk('images')->url($value))
             : null;
     }
 }
