9681800ce2a1f8a1c1d4530467eaa3764d573e66
Faiz Ahmad <faizahmadse@gmail.com>
2020-07-21 13:19:27 +0500

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -70,14 +70,14 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'user@membership.digital'
         ]);
-        $members = factory(App\User::class, 19)->create()->pluck('id')->toArray();
+        $members = factory(App\User::class, 1)->create()->pluck('id')->toArray();
         $organization =  factory(Organization::class)->create([
             'name' => 'Organization',
             'email' => 'organization@membership.digital'
         ]);
-        $organization->announcements()->saveMany(factory(Announcement::class, 19)->create());
-        $organization->benefits()->saveMany(factory(Benefit::class, 19)->create());
-        $organization->notifications()->saveMany(factory(Notification::class, 19)->create());
+        $organization->announcements()->saveMany(factory(Announcement::class, 1)->create());
+        $organization->benefits()->saveMany(factory(Benefit::class, 1)->create());
+        $organization->notifications()->saveMany(factory(Notification::class, 1)->create());
         $organization->members()->attach($members, ['status' => 'accepted']);
         $organization->members()->attach($test_user->id, ['status' => 'accepted']);
     }
