2eee6ca7f816cd5017d73866af84f58356ff9a44
Faiz Ahmad <faizahmadse@gmail.com>
2020-07-21 13:46:12 +0500

    Update NovaServiceProvider.php

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -61,7 +61,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     {
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
-                'admin@membershipdigital.my'
+                'admin@membership.digital'
             ]);
         });
     }
