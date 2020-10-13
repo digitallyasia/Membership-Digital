b764969035018ebf7b8a5afdd72b425aafb1ec74
Faiz Ahmad <faizahmadse@gmail.com>
2020-10-13 13:30:29 +0500

    Update TelescopeServiceProvider.php

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -64,7 +64,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     {
         Gate::define('viewTelescope', function ($user) {
             return in_array($user->email, [
-                'admin@membership.digital'
+                'faizahmadse@gmail.com'
             ]);
         });
     }
