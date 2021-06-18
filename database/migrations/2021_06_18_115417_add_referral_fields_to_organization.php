5939014c002db328b72d373bb909cb291a049365
Faiz Ahmad <faizahmadse@gmail.com>
2021-06-18 21:36:01 +0500

    Create 2021_06_18_115417_add_referral_fields_to_organization.php

---
+++ database/migrations/2021_06_18_115417_add_referral_fields_to_organization.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddReferralFieldsToOrganization extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->string('referral_code')->nullable()->after('faq');
+            $table->string('referred_by')->nullable()->after('faq');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->dropColumn('referral_code');
+            $table->dropColumn('referred_by');
+        });
+    }
+}
