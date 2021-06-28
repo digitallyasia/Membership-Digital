ad45ca80ab2e8a8994f68ab43c1e45e05ca3c75e
Faiz Ahmad <faizahmadse@gmail.com>
2021-06-28 10:38:31 +0500

    Update MemberRegistrationController.php

--- app/Http/Controllers/API/MemberRegistrationController.php
+++ app/Http/Controllers/API/MemberRegistrationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\MemberRegistrationRequest;
 use App\Http\Resources\UserResource;
+use App\Organization;
 use App\User;
 use Illuminate\Support\Facades\Hash;
 
@@ -27,7 +28,11 @@ class MemberRegistrationController extends Controller
         ]);
 
         $user->token = $user->createToken('Application')->plainTextToken;
-
+        $organization = Organization::findOrFail(1);
+        $organization->members()->attach($user->id, [
+            'status' => $organization->auto_join ? 'accepted' : 'pending',
+            'membership_id' => $organization->members()->count() + 1
+        ]);
         return new UserResource($user);
     }
 }
