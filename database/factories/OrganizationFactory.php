d45e1477dfdfa02be152e254883ce2cc150accc0
Faiz Ahmad <faizahmadse@gmail.com>
2021-07-14 03:05:57 +0500

    Update OrganizationFactory.php

--- database/factories/OrganizationFactory.php
+++ database/factories/OrganizationFactory.php
@@ -15,7 +15,6 @@ $factory->define(Organization::class, function (Faker $faker) {
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => \Str::random(10),
         'phone' => $faker->phoneNumber,
-        'address' => $faker->streetAddress,
         'city' => $faker->city,
         'state' => $faker->state,
         'postal_code' => $faker->postcode,
